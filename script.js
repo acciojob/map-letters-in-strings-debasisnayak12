5fb6bfad0246f31d9f5f77e89c8e6aea9aadbd8f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-21 10:42:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(word) {
+	let obj = {};
+	word.split('').forEach((letter,i) => {
+		if(/[a-zA-Z]/.test(letter)){
+			if(!(letter in obj)){
+				obj[letter] = [];
+			}
+			obj[letter].push(i);
+		}
+	});
+	return obj;
+}
+const word = prompt("Enter a word");
+console.log(mapLetters(word));
